5d1758a24d0ec968007889ecc952e818ba4afb8d
Chris Garrett <z@zworkbench.com>
2015-11-11 10:04:44 -0500

    Created Blank iOS project

---
+++ NameTag/NameTag/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NameTag
+//
+//  Created by Christopher Garrett on 11/11/15.
+//  Copyright © 2015 ZWorkbench. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
